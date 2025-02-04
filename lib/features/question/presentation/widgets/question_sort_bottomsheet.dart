337abdfe4bbe434e64b269c96146e44ec0750c91
gourav0sharma1 <gourav0sharma1@gmail.com>
2025-02-04 17:59:03 +0000

    fix: filter sort option bottomsheet getting reset on bottomsheet dismiss

--- lib/features/question/presentation/widgets/question_sort_bottomsheet.dart
+++ lib/features/question/presentation/widgets/question_sort_bottomsheet.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:codersgym/features/question/domain/model/problem_sort_option.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -12,93 +14,99 @@ class QuestionSortBottomsheet extends HookWidget {
   Widget build(BuildContext context) {
     final selectedSortOption = useState<ProblemSortOption?>(initialValue);
     final isAscending = useState<bool>(true);
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 16,
-        horizontal: 16,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Sort by",
-                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    selectedSortOption.value = null;
-                  },
-                  child: Text(
-                    "Reset",
-                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) return;
+        Navigator.of(context).pop(initialValue);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Sort by",
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
                           fontWeight: FontWeight.bold,
-                          color: Theme.of(context).primaryColor,
                         ),
                   ),
-                )
-              ],
+                  TextButton(
+                    onPressed: () {
+                      selectedSortOption.value = null;
+                    },
+                    child: Text(
+                      "Reset",
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          _buildSortOption(
-            context,
-            'Title',
-            Icons.sort_by_alpha,
-            selectedSortOption.value,
-            ProblemSortOption.titleAsc,
-            ProblemSortOption.titleDesc,
-            selectedSortOption,
-            isAscending,
-          ),
-          _buildSortOption(
-            context,
-            'Acceptance',
-            Icons.percent,
-            selectedSortOption.value,
-            ProblemSortOption.acceptanceRateAsc,
-            ProblemSortOption.acceptanceRateDesc,
-            selectedSortOption,
-            isAscending,
-          ),
-          _buildSortOption(
-            context,
-            'Difficulty',
-            Icons.bar_chart,
-            selectedSortOption.value,
-            ProblemSortOption.difficultyAsc,
-            ProblemSortOption.difficultyDesc,
-            selectedSortOption,
-            isAscending,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context, initialValue);
-                },
-                child: const Text('Cancel'),
-              ),
-              const SizedBox(
-                width: 12,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context, selectedSortOption.value);
-                },
-                child: const Text("Apply"),
-              ),
-            ],
-          )
-        ],
+            _buildSortOption(
+              context,
+              'Title',
+              Icons.sort_by_alpha,
+              selectedSortOption.value,
+              ProblemSortOption.titleAsc,
+              ProblemSortOption.titleDesc,
+              selectedSortOption,
+              isAscending,
+            ),
+            _buildSortOption(
+              context,
+              'Acceptance',
+              Icons.percent,
+              selectedSortOption.value,
+              ProblemSortOption.acceptanceRateAsc,
+              ProblemSortOption.acceptanceRateDesc,
+              selectedSortOption,
+              isAscending,
+            ),
+            _buildSortOption(
+              context,
+              'Difficulty',
+              Icons.bar_chart,
+              selectedSortOption.value,
+              ProblemSortOption.difficultyAsc,
+              ProblemSortOption.difficultyDesc,
+              selectedSortOption,
+              isAscending,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, initialValue);
+                  },
+                  child: const Text('Cancel'),
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, selectedSortOption.value);
+                  },
+                  child: const Text("Apply"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -113,8 +121,8 @@ class QuestionSortBottomsheet extends HookWidget {
     ValueNotifier<ProblemSortOption?> selectedSortOption,
     ValueNotifier<bool> isAscending,
   ) {
-    bool isSelected = selectedSortOption.value == ascOption ||
-        selectedSortOption.value == descOption;
+    bool isSelected =
+        selectedOption == ascOption || selectedOption == descOption;
 
     return ListTile(
       leading: Icon(icon),
@@ -126,7 +134,7 @@ class QuestionSortBottomsheet extends HookWidget {
             )
           : null,
       onTap: () {
-        if (selectedSortOption.value == ascOption) {
+        if (selectedOption == ascOption) {
           selectedSortOption.value = descOption;
           isAscending.value = false;
         } else {
