dbbc0635db102eee34a1d85633c2aef653881d57
gourav0sharma1 <gourav0sharma1@gmail.com>
2025-02-05 17:15:28 +0000

    feat: add streak animation in home page

--- lib/features/profile/presentation/widgets/leetcode_streak_fire.dart
+++ lib/features/profile/presentation/widgets/leetcode_streak_fire.dart
@@ -1,8 +1,13 @@
-import 'package:codersgym/features/profile/domain/model/user_profile.dart';
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class LeetCodeStreakFire extends StatelessWidget {
+import 'package:codersgym/features/profile/domain/model/user_profile.dart';
+import 'package:codersgym/gen/assets.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:focus_detector/focus_detector.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:lottie/lottie.dart';
+
+class LeetCodeStreakFire extends HookWidget {
   final StreakCounter streakCounter;
 
   const LeetCodeStreakFire({
@@ -12,25 +17,50 @@ class LeetCodeStreakFire extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final lottieController = useAnimationController();
     final streakCount = streakCounter.streakCount;
     final theme = Theme.of(context);
     final streakColor = streakCounter.currentDayCompleted
         ? theme.primaryColor
         : theme.hintColor;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          FontAwesomeIcons.fire,
-          color: streakColor,
-          size: 18,
-        ),
-        const SizedBox(width: 8.0),
-        Text(
-          '$streakCount',
-          style: theme.textTheme.titleLarge?.copyWith(color: streakColor),
-        ),
-      ],
+    return FocusDetector(
+      onFocusGained: () {
+        if (lottieController.duration != null) {
+          lottieController
+            ..reset()
+            ..forward();
+        }
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          streakCounter.currentDayCompleted
+              ? Lottie.asset(
+                  Assets.lotties.flame.path,
+                  height: 28,
+                  width: 28,
+                  repeat: false,
+                  controller: lottieController,
+                  onLoaded: (composition) {
+                    // Configure the AnimationController with the duration of the
+                    // Lottie file and start the animation.
+                    lottieController
+                      ..duration = composition.duration
+                      ..forward();
+                  },
+                )
+              : Icon(
+                  FontAwesomeIcons.fire,
+                  color: streakColor,
+                  size: 24,
+                ),
+          const SizedBox(width: 8.0),
+          Text(
+            '$streakCount',
+            style: theme.textTheme.titleLarge?.copyWith(color: streakColor),
+          ),
+        ],
+      ),
     );
   }
 }
